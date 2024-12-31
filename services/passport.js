acf14e1f68eb6e1b120ee34fca7b88a48f4df672
Cole Freeman <cole.f.freeman@gmail.com>
2024-12-31 11:57:18 -0500

    commit encoding users to mongodb

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('User');
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +13,18 @@ passport.use(
             callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile', profile);
+            User.findOne({ googleID: profile.id })
+                .then((existingUser) => {
+                    if (existingUser) {
+                        // we already have a record with the diven profile ID
+                        done(null, existingUser);
+                    } else {
+                        // we don't have a user record with this ID so make a new one
+                        new User({ googleID: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                })
         }
     )
 );
